6dcd3596fc252182d3513ed61db416926a6a343b
Lenovo IdeaPad <Lenovo IdeaPad>
2016-11-20 12:13:42 +0000

    Added play button

---
+++ app/src/main/java/titanstudio/mashd/Mashd.java
@@ -0,0 +1,13 @@
+package titanstudio.mashd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mashd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mashd);
+    }
+}
